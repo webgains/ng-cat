c1b8eaf7224f8e42e3e7e7f8c3140061a923cb4a
Davide Cavaliere <cavaliere.davide@gmail.com>
2015-03-29 12:09:57 +0100

    first commit

---
+++ client/app/main/main.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('ngCatApp')
+  .config(function ($stateProvider) {
+    $stateProvider
+      .state('main', {
+        url: '/',
+        templateUrl: 'app/main/main.html',
+        controller: 'MainCtrl'
+      });
+  });
